04d3f461b9b63d51f3f7ecf1daa753adc75243f0
cengiz <cengiz.ergun.0101@gmail.com>
2023-07-11 21:44:20 +0300

    tool: storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <main className="flex justify-center items-center h-screen">Main Page</main>
+  return (
+    <main className="flex justify-center items-center h-screen">Main Page</main>
+  )
 }
